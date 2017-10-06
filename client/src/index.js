73c6b92bbe20eea5fd364b5d1356f358696ee171
mfrachet <marvin.frachet@gmail.com>
2017-10-06 08:09:29 +0200

    Removing ionicons

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bulma/css/bulma.css";
-import "ionicons/dist/css/ionicons-core.min.css";
-import "ionicons/dist/css/ionicons.min.css";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
