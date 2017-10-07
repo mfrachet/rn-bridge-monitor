c15864fe99ac95c6ea07f3f6cd97d3ff15715a63
mfrachet <marvin.frachet@gmail.com>
2017-10-07 17:04:39 +0200

    Fix lint && prettier

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,23 +1,18 @@
 #!/usr/bin/env node
-const startServer = require("rn-bridge-monitor-server");
-const yargs = require("yargs");
+const startServer = require('rn-bridge-monitor-server');
+const yargs = require('yargs');
 
 const DEFAULT_PORT = 5000;
 
-const defineOptions = args => {
-  args.option("p", {
-    describe: "The port to be used while running the web/socket server",
-    default: DEFAULT_PORT
+const defineOptions = (args) => {
+  args.option('p', {
+    describe: 'The port to be used while running the web/socket server',
+    default: DEFAULT_PORT,
   });
 };
 
-const playCommand = argv => {
+const playCommand = (argv) => {
   startServer(argv.p);
 };
 
-module.exports = yargs.command(
-  "start",
-  "start the server",
-  defineOptions,
-  playCommand
-).argv;
+module.exports = yargs.command('start', 'start the server', defineOptions, playCommand).argv;
