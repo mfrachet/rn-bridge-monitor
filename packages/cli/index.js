7b86f00ec0341368994f4842eff73965224cab09
mfrachet <marvin.frachet@gmail.com>
2017-10-09 08:05:07 +0200

    Adding server and client to ui

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const startServer = require('rn-bridge-monitor-server');
+const startServer = require('rn-bridge-monitor-ui');
 const yargs = require('yargs');
 
 const DEFAULT_PORT = 5000;
